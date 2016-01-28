64ba4303ae14b4d5e9b0896bc973d9332ab7160d
nbingo <nringach1@hwemail.com>
2016-01-28 15:26:01 -0800

    A fly can have two alleles for one gene now, whic makes sense

--- FlyLab/src/Fly.java
+++ FlyLab/src/Fly.java
@@ -3,18 +3,18 @@ import java.awt.*;
 
 public class Fly 
 {
-	Bristle   bristle  ;
-	Color     eyeColor ;
-	Color     bodyColor;
-	Antenna   antenna  ;
-	EyeShape  eyeShape ;
-	WingSize  wingSize ;
-	WingShape wingShape;
-	WingVein  wingVein ;
-	WingAngle wingAngle;
-	Gender    gender   ;
+	Bristle  [] bristle  ;
+	Color    [] eyeColor ;
+	Color    [] bodyColor;
+	Antenna  [] antenna  ;
+	EyeShape [] eyeShape ;
+	WingSize [] wingSize ;
+	WingShape[] wingShape;
+	WingVein [] wingVein ;
+	WingAngle[] wingAngle;
+	Gender   [] gender   ;
 	
-	public Fly (Bristle b, Color eyeC, Color bodyC, Antenna a, EyeShape eye, WingSize wSize, WingShape wShape, WingVein wVein, WingAngle wAngle, Gender g)
+	public Fly (Bristle[] b, Color[] eyeC, Color[] bodyC, Antenna[] a, EyeShape[] eye, WingSize[] wSize, WingShape[] wShape, WingVein[] wVein, WingAngle[] wAngle, Gender[] g)
 	{
 		bristle   = b;
 		eyeColor  = eyeC;
@@ -28,12 +28,23 @@ public class Fly
 		gender    = g;
 	}
 	
-	public Fly(Gender g)
+	public Fly(Gender[] g)
 	{
-		this(Bristle.Wild, Color.RED, Color.ORANGE, Antenna.Wild, EyeShape.Wild, WingSize.Wild, WingShape.Wild, WingVein.Wild, WingAngle.Wild, g);
+		this(
+				new Bristle  [] {Bristle.Wild  , Bristle.Wild  }, 
+				new Color    [] {Color.RED     , Color.RED     }, 
+				new Color    [] {Color.ORANGE  , Color.ORANGE  },
+				new Antenna  [] {Antenna.Wild  , Antenna.Wild  },
+				new EyeShape [] {EyeShape.Wild , EyeShape.Wild },
+				new WingSize [] {WingSize.Wild , WingSize.Wild },
+				new WingShape[] {WingShape.Wild, WingShape.Wild},
+				new WingVein [] {WingVein.Wild , WingVein.Wild },
+				new WingAngle[] {WingAngle.Wild, WingAngle.Wild}, 
+				g
+				);
 	}
 	public Fly()
 	{
-		this(Gender.Female);
+		this(new Gender[] {Gender.X, Gender.X});
 	}
 }
