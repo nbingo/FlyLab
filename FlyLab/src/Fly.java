c3514eafef283f1722119569b1d8c14b649a35d7
nbingo <nringach1@hwemail.com>
2016-01-28 16:48:31 -0800

    Commented methods and classes, which was probably a good idea

--- FlyLab/src/Fly.java
+++ FlyLab/src/Fly.java
@@ -1,6 +1,11 @@
 import java.awt.*;
 
-
+/**
+ * The class for creating flies and with certain genotypes.
+ * NB: a null in one of the genotypes indicates that there is only one allele of that gene in the fly, such as in males with a gene on the X chromosome. 
+ * Of course, there will only be one null max per gene.
+ * @version 1.0.0
+ */
 public class Fly 
 {
 	Bristle  [] bristle  ;
@@ -14,6 +19,20 @@ public class Fly
 	WingAngle[] wingAngle;
 	Gender   [] gender   ;
 	
+	/**
+	 * Creates a Fly based on the genotype given. All parameters must be arrays of length two so as to represent two alleles.
+	 * 
+	 * @param b Bristle type
+	 * @param eyeC Eye color
+	 * @param bodyC Body color
+	 * @param a Antenna type
+	 * @param eye Eye shape/size
+	 * @param wSize Wing size
+	 * @param wShape Wing shape
+	 * @param wVein Type of veins on wings
+	 * @param wAngle Angle of wings
+	 * @param g Gender (as in XX or XY)
+	 */
 	public Fly (Bristle[] b, Color[] eyeC, Color[] bodyC, Antenna[] a, EyeShape[] eye, WingSize[] wSize, WingShape[] wShape, WingVein[] wVein, WingAngle[] wAngle, Gender[] g)
 	{
 		bristle   = b;
@@ -28,6 +47,10 @@ public class Fly
 		gender    = g;
 	}
 	
+	/**
+	 * Creates a default fly of the specified gender.
+	 * @param g The gender (as in XX or XY)
+	 */
 	public Fly(Gender[] g)
 	{
 		this(
@@ -43,6 +66,10 @@ public class Fly
 				g
 				);
 	}
+	
+	/**
+	 * Creates a default female fly.
+	 */
 	public Fly()
 	{
 		this(new Gender[] {Gender.X, Gender.X});
--- FlyLab/src/Stats.java
+++ FlyLab/src/Stats.java
@@ -1,5 +1,12 @@
 
 public class Stats {
+	
+	/**
+	 * Performs the chi^2 test on a list of values. Will return the % chance that the deviation was due to random chance in the future.
+	 * @param observed a list of observed values
+	 * @param expected a list of expected values corresponding to the observed values
+	 * @return the chi^2 test of the given observed and expected values
+	 */
 	public static double chiSquared(double[] observed, double[] expected)
 	{
 		int sum = 0;
@@ -8,6 +15,12 @@ public class Stats {
 		return sum;
 	}
 	
+	/**
+	 * Calculates the chi^2 of a single observed and expected value using: (o-e)^2 / e.
+	 * @param observed the observed value
+	 * @param expected the expected value
+	 * @return the single value for the chi^2 of the given observed and expected values
+	 */
 	private static double singleChiSquared(double observed, double expected)
 	{
 		if (expected == 0)
